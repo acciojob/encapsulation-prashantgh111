aed0fad4a4a5ad84bdaa9e34f496efc78877945e
prashantgh111 <107759833+prashantgh111@users.noreply.github.com>
2023-01-27 23:11:30 +0530

    Solution Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String [] args){
+        RWOnly p = new RWOnly();
+//        p.name="prashant";
+//        System.out.println(p);   // name has private access in com.driver.RWOnly
+        p.setName("prashant");
+        p.getName();     // only we have to call it no need to print anything.
+
+    }
 }
